bb44dd9c513b5a58e72f7223b8ba9f1115345e67
tecnicocefet <tecnicocefet@gmail.com>
2024-03-04 07:31:04 -0300

    first commit

--- main.tf
+++ main.tf
@@ -10,15 +10,22 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
-  key_name = "iac-alura"
-
+  key_name      = "iac-alura"
+  
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito com o Terraform</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #             EOF
+  
   tags = {
-    Name = "Primeira Instancia"
+    Name = "Terraform Ansible Python"
   }
 }
